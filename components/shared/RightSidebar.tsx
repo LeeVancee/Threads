a9783bb284f92a8022567643c18ec4403a975295
LeeVancee <1280403599@qq.com>
2023-08-07 04:54:34 +0800

    add Profile

---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const RightSiderbar = () => {
+  return (
+    <section className="custom-scrollbar rightsidebar">
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-light-1 text-heading4-medium">
+          Suggest Comumnities
+        </h3>
+      </div>
+      <div className="flex flex-1 flex-col justify-start">
+        <h3 className="text-light-1 text-heading4-medium">Suggest Users</h3>
+      </div>
+    </section>
+  );
+};
+
+export default RightSiderbar;
---
+++ lib/models/user.model.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: String,
+  bio: String,
+  threads: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thread",
+    },
+  ],
+  onboarded: {
+    type: Boolean,
+    default: false,
+  },
+  communities: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Community",
+    },
+  ],
+});
+
+// 如果模型尚未定義，則定義它
+const User = mongoose.models.User || model("User", userSchema);
+
+export default User;
